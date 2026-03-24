3c39809c117e4c69ae76bd3822f9d7644a4b28ee
bubblegump <mos131998@gmail.com>
2026-03-24 16:06:58 +0700

    Feature/payment (#2)
    * feat(payment):add folder component and features create file payment
    
    * fix(payment):fix import styles globalcss
    
    * feat: initial commit
    
    * feat(payment):add payment page and studentinfo header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 
-import "./globals.css";
+import { cn } from "@/lib/utils";
+import { Inter } from "next/font/google";
+import { poppins } from "./styles/font";
+import "./styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: {
@@ -15,8 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+    <html lang="en" className={cn("font-sans", inter.variable)}>
+      <body className={`antialiased ${poppins.className}`}>{children}</body>
     </html>
   );
 }
