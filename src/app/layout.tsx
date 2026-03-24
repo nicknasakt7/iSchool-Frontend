1287f67ec133fcb5af5a9eaadcbd2f0cc9456f4b
Pratchaya007 <pratchaya.1012@gmail.com>
2026-03-24 17:32:30 +0700

    fix:(layout): remove code duplicate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { Inter } from "next/font/google";
 
 
 import { cn } from "@/lib/utils";
@@ -11,15 +11,7 @@ import { Poppins } from "next/font/google";
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -42,15 +34,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    
     <html lang="en" className={cn("font-sans", inter.variable)}>
-      <body className={`antialiased ${poppins.className}`}>{children}</body>
-    <html lang="en">
-      <body>
+      <body className={`antialiased ${poppins.className}`}>{children}
+ 
         <Navbar/>
         
         {children}
