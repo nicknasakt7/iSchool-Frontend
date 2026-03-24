9a45c26cf895b163ce64b69ec31435e3633d0ee9
Pratchaya007 <pratchaya.1012@gmail.com>
2026-03-24 11:19:14 +0700

    feat: initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,27 @@
 import type { Metadata } from "next";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
+
 
 import { cn } from "@/lib/utils";
 
 import "./styles/globals.css";
 import { poppins } from "./styles/font";
-import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+
+
+
 
 export const metadata: Metadata = {
   title: {
@@ -21,6 +36,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <html
+      lang="en"
+      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
     <html lang="en" className={cn("font-sans", inter.variable)}>
       <body className={`antialiased ${poppins.className}`}>{children}</body>
     </html>
