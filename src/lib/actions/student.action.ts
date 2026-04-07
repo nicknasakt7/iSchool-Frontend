3ab4e5a8ca5574e9032fabd99e7c0a228bff517e
nicknasakt7 <tntkt7@hotmail.com>
2026-04-07 07:25:01 +0700

    refactor(student-action):Update import path

--- src/lib/actions/student.action.ts
+++ src/lib/actions/student.action.ts
@@ -1,16 +1,16 @@
-"use server";
+'use server';
 
-import { StudentFormValues } from "@/components/features/create/form/NewEntryForm";
-import { studentService } from "../api/student/student.service";
-import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
+import { studentService } from '../api/student/student.service';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import { StudentFormValues } from '../schemas/student.schema';
 
 export const createStudent = async (input: StudentFormValues) => {
   try {
     await studentService.createStudent(input);
-    revalidatePath("/create/new-entry");
+    revalidatePath('/create/new-entry');
   } catch (error) {
-    console.log("error", error);
+    console.log('error', error);
   }
-  redirect("/create/new-entry");
+  redirect('/create/new-entry');
 };
