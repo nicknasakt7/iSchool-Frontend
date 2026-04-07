b2c8e0ad40875a7c77dab588688cb04c359afde7
pariya-v <upariya.vor@gmail.com>
2026-04-07 15:37:29 +0700

    feat(forgot-password): fix confilct after rebase dev

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,33 +1,19 @@
-import { auth } from '@/lib/auth/auth';
-import { NextResponse } from 'next/server';
+import { auth } from "@/lib/auth/auth";
+import { NextResponse } from "next/server";
 
 // const protectedRoutes = ['/dashboard', '/students', '/assessments', '/checkin' ,'/create/new-entry' , '/create/new-admin'];
 const publicRoutes = ["/", "/login", "/reset-password"];
 const teacherRoutes = [
-  '/dashboard',
-  '/students',
-  '/students/[student-id]',
-  '/students/undefined/edit',
-  '/assessments',
-  '/checkin',
-  '/create/new-entry',
   "/dashboard",
   "/students",
   "/students/[student-id]",
-  "/students/undefined/edit",
+  "/students/[student-id]/[edit-student-id]",
   "/assessments",
   "/checkin",
   "/create/new-entry",
 ];
 const adminRoutes = [
-  '/',
-  '/dashboard',
-  '/students',
-  '/students/[student-id]',
-  '/students/[student-id]/[edit-student-id]',
-  '/assessments',
-  '/checkin',
-  '/create/new-entry',
+  "/",
   "/dashboard",
   "/students",
   "/students/[student-id]",
@@ -37,28 +23,22 @@ const adminRoutes = [
   "/create/new-entry",
 
   // admin-management
-  '/admin-managements/enrollments',
+  "/admin-managements/enrollments",
 
-  '/admin-managements/teachers',
+  "/admin-managements/teachers/teacher-managements",
+  "/admin-managements/teachers/new-teacher",
 
-  '/admin-managements/relations',
+  "/admin-managements/relations",
 
-  '/admin-managements/academic-setup/grade-architecture',
-  '/admin-managements/academic-setup/create-subject',
-  '/admin-managements/academic-setup/students-promotion',
+  "/admin-managements/academic-setup/grade-architecture",
+  "/admin-managements/academic-setup/create-subject",
+  "/admin-managements/academic-setup/students-promotion",
 
-  '/admin-managements/finance/fee-management',
-  '/admin-managements/finance/create-transaction',
+  "/admin-managements/finance/fee-management",
+  "/admin-managements/finance/create-transaction",
 ];
 const superAdminRoutes = [
-  '/',
-  '/dashboard',
-  '/students',
-  '/students/[student-id]',
-  '/students/[student-id]/edit',
-  '/assessments',
-  '/checkin',
-  '/create/new-entry',
+  "/",
   "/dashboard",
   "/students",
   "/students/[student-id]",
@@ -68,42 +48,37 @@ const superAdminRoutes = [
   "/create/new-entry",
 
   // admin-management
-  '/admin-managements/enrollments',
+  "/admin-managements/enrollments",
 
-  '/admin-managements/teachers',
+  "/admin-managements/teachers/teacher-managements",
+  "/admin-managements/teachers/new-teacher",
 
-  '/admin-managements/relations',
+  "/admin-managements/relations",
 
-  '/admin-managements/academic-setup/grade-architecture',
-  '/admin-managements/academic-setup/create-subject',
-  '/admin-managements/academic-setup/students-promotion',
+  "/admin-managements/academic-setup/grade-architecture",
+  "/admin-managements/academic-setup/create-subject",
+  "/admin-managements/academic-setup/students-promotion",
 
-  '/admin-managements/finance/fee-management',
-  '/admin-managements/finance/create-transaction',
+  "/admin-managements/finance/fee-management",
+  "/admin-managements/finance/create-transaction",
 
   // super admin only
-  '/create/new-admin',
+  "/create/new-admin",
 ];
-const parents = ['/', '/parents/student-info', '/parents/payment'];
-const parents = ["/parents/student-info", "parents/payment"];
+const parents = ["/", "/parents/student-info", "/parents/payment"];
 
 const ROLE = {
-  SUPER_ADMIN: 'SUPER_ADMIN',
-  ADMIN: 'ADMIN',
-  TEACHER: 'TEACHER',
-  PARENTS: 'PARENTS',
+  SUPER_ADMIN: "SUPER_ADMIN",
+  ADMIN: "ADMIN",
+  TEACHER: "TEACHER",
+  PARENTS: "PARENTS",
 };
 
-export const proxy = auth(req => {
+export const proxy = auth((req) => {
   const pathname = req.nextUrl.pathname;
   const isAuthenticated = !!req.auth;
   const role = req.auth?.user?.role;
 
-  if (!isAuthenticated) {
-    if (!publicRoutes.includes(pathname)) {
-      return NextResponse.redirect(new URL('/', req.url));
-    } else {
-      return NextResponse.next();
   if (!isAuthenticated) {
     if (!publicRoutes.includes(pathname)) {
       return NextResponse.redirect(new URL("/", req.url));
@@ -112,16 +87,16 @@ export const proxy = auth(req => {
     }
   }
 
-  if (pathname === '/login' && isAuthenticated) {
+  if (pathname === "/login" && isAuthenticated) {
     if (role === ROLE.PARENTS) {
-      return NextResponse.redirect(new URL('/parents/student-info', req.url));
+      return NextResponse.redirect(new URL("/parents/student-info", req.url));
     }
-    return NextResponse.redirect(new URL('/dashboard', req.url));
+    return NextResponse.redirect(new URL("/dashboard", req.url));
   }
 
-  //Teacher
-  const isTeacherRoute = teacherRoutes.some(el =>
-    el === '/' ? pathname === el : pathname.startsWith(el),
+  // Teacher
+  const isTeacherRoute = teacherRoutes.some((el) =>
+    el === "/" ? pathname === el : pathname.startsWith(el),
   );
 
   if (isTeacherRoute && role === ROLE.TEACHER) {
@@ -129,50 +104,41 @@ export const proxy = auth(req => {
   }
 
   // Admin
-  const isAdminRoute = adminRoutes.some(el =>
-    el === '/' ? pathname === el : pathname.startsWith(el),
+  const isAdminRoute = adminRoutes.some((el) =>
+    el === "/" ? pathname === el : pathname.startsWith(el),
   );
 
   if (isAdminRoute && role === ROLE.ADMIN) {
     return NextResponse.next();
   }
 
-  //super_admin
-  const isSuperAdminRoutes = superAdminRoutes.some(el =>
-    el === '/' ? pathname === el : pathname.startsWith(el),
-  );
+  // super_admin
   const isSuperAdminRoutes = superAdminRoutes.some((el) =>
     el === "/" ? pathname === el : pathname.startsWith(el),
   );
 
   if (isSuperAdminRoutes && role === ROLE.SUPER_ADMIN) {
-  if (isSuperAdminRoutes && role === ROLE.SUPER_ADMIN) {
     return NextResponse.next();
   }
 
-  //PARENTS
-  const isParentsRoutes = parents.some(el =>
-    el === '/' ? pathname === el : pathname.startsWith(el),
-  );
+  // PARENTS
   const isParentsRoutes = parents.some((el) =>
     el === "/" ? pathname === el : pathname.startsWith(el),
   );
 
   if (isParentsRoutes && role === ROLE.PARENTS) {
     return NextResponse.next();
-  if (isParentsRoutes && role === ROLE.PARENTS) {
-    return NextResponse.next();
   }
 
   if (role === ROLE.PARENTS) {
-    return NextResponse.redirect(new URL('/parents/student-info', req.url));
+    return NextResponse.redirect(new URL("/parents/student-info", req.url));
   }
 
-  return NextResponse.redirect(new URL('/dashboard', req.url));
+  return NextResponse.redirect(new URL("/dashboard", req.url));
 });
 
 export const config = {
   matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:png|jpg|jpeg|webp|gif|svg|ico)).*)',
+    "/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:png|jpg|jpeg|webp|gif|svg|ico)).*)",
   ],
 };
