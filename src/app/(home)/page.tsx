6fd85c3bb24d345d5ae67aecc162b2af8413106d
Jutamat Jarusirisakul <tntkt7@hotmail.com>
2026-04-11 23:24:58 +0700

    Feature/enrollment hub (#48)
    * feat(interested-form/enrollment-hub): Implement interested-form/enrollment-hub
    
    * feat(interested-form):Add texts after submit form

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,12 +3,12 @@ import ContentHome from '@/components/content/content';
 import ContentAcademic from '@/components/content/contentAcademic';
 import ContentPillars from '@/components/content/contentpillars';
 import Footer from '@/components/features/homepage/footer/footer';
-import FormRegister from '@/components/features/homepage/form/formregister';
+import InterestedForm from '@/components/features/homepage/form/interested-form';
 
 export default function HomePage() {
   return (
     <>
-      <div className="bg-[#fcfcfc] min-h-screen">
+      <div className="bg-card min-h-screen">
         <div className="mx-auto w-full max-w-sm sm:max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-5xl space-y-6">
           {/* content main */}
           <ContentHome />
@@ -19,7 +19,7 @@ export default function HomePage() {
           {/* content academic */}
           <ContentAcademic />
           {/* Form contect*/}
-          <FormRegister />
+          <InterestedForm />
         </div>
         <Footer />
       </div>
