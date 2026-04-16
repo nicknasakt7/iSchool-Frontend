a4f1a6f8b336ed8d7f434715ed29770707090526
pariya-v <upariya.vor@gmail.com>
2026-04-16 18:26:22 +0700

    feat(invite-parent): Fix resolve student directory pagination reset and invite URL and implement forgot password flow

--- src/lib/api/invite/invite.service.ts
+++ src/lib/api/invite/invite.service.ts
@@ -2,6 +2,6 @@ import { api } from "../api-server";
 
 export const inviteService = {
   inviteParent: (email: string) => {
-    return api.post("/invite/parent", { email });
+    return api.post("/invites/parent", { email });
   },
 };
